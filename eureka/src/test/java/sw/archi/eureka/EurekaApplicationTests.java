f8b372471b43babb117d6bddd0a2e412d9913185
pikapikapikaori <pikapikapi_kaori@icloud.com>
2024-05-14 23:39:32 +0800

    config: generate project

---
+++ eureka/src/test/java/sw/archi/eureka/EurekaApplicationTests.java
@@ -0,0 +1,11 @@
+package sw.archi.eureka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaApplicationTests {
+
+    @Test
+    void contextLoads() {}
+}
