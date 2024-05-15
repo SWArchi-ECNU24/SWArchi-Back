7fb5b2ce619e9b0b036754bf888602d143ffb93e
pikapikapi <pikapikapi_kaori@icloud.com>
2024-05-15 18:54:05 +0800

    config: add common

---
+++ common-utils/src/main/java/sw/archi/commonutils/constants/SWConstants.java
@@ -0,0 +1,20 @@
+package sw.archi.commonutils.constants;
+
+public class SWConstants {
+
+    public static String code = "code";
+
+    public static String data = "data";
+
+    public static String projectPackageName = "sw.archi";
+
+    public static String authModulePackageName = "auth";
+
+    public static String confjourModulePackageName = "conferencejournal";
+
+    public static String daoClassPackageName = "dao";
+
+    public static String daoClassSuffix = "Repository";
+
+    public static String findByIdMethodName = "findById";
+}
