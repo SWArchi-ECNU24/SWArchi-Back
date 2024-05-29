e05bbac9225e87c49fff21e06c538fb7f2a66523
pikapikapi <pikapikapi_kaori@icloud.com>
2024-05-29 11:27:44 +0800

    refactor: refactor intermediator controller

--- intermediator/src/main/java/sw/archi/intermediator/controller/GeneralController.java
+++ intermediator/src/main/java/sw/archi/intermediator/controller/GeneralController.java
@@ -2,6 +2,7 @@ package sw.archi.intermediator.controller;
 
 import com.alibaba.fastjson.JSONObject;
 import io.swagger.v3.oas.annotations.Parameter;
+import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
@@ -22,12 +23,28 @@ public class GeneralController {
     @Autowired
     private GeneralService generalService;
 
-    @GetMapping(value = "/auth/{tableName}/{id}")
+    private Map.Entry<String, String> getModuleInfo(String moduleName) {
+        switch (moduleName) {
+            case "auth":
+                return Map.entry(IMConstants.authModuleBaseUrl, IMConstants.authModuleApiPrefix);
+            case "confjour":
+                return Map.entry(IMConstants.confjourModuleBaseUrl, IMConstants.confjourModuleApiPrefix);
+            default:
+                return Map.entry(null, null);
+        }
+    }
+
+    @GetMapping(value = "/{moduleName}/{tableName}/{id}")
     public ResponseEntity<JSONObject> getAuthByTableNId(
+            @Parameter(description = "moduleName") @PathVariable String moduleName,
             @Parameter(description = "Id") @PathVariable int id,
             @Parameter(description = "tableName") @PathVariable String tableName) {
         return generalService
-                .getDataById(IMConstants.authModuleBaseUrl, IMConstants.authModuleApiPrefix, tableName, id)
+                .getDataById(
+                        getModuleInfo(moduleName).getKey(),
+                        getModuleInfo(moduleName).getValue(),
+                        tableName,
+                        id)
                 .toResponseEntity();
     }
 }
