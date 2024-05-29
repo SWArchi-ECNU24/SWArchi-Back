0159f4f597a22d4aec6f06e4da34062ebfa045c8
pikapikapi <pikapikapi_kaori@icloud.com>
2024-05-29 19:09:49 +0800

    feat: add inter swagger

--- intermediator/src/main/java/sw/archi/intermediator/IntermediatorApplication.java
+++ intermediator/src/main/java/sw/archi/intermediator/IntermediatorApplication.java
@@ -1,12 +1,23 @@
 package sw.archi.intermediator;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@OpenAPIDefinition(info = @Info(title = "中间层模块", description = "中间层模块 Intermediator"))
 @EnableDiscoveryClient
 @SpringBootApplication
 public class IntermediatorApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(IntermediatorApplication.class, args);
