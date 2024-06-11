bd6782ef62d1dae7b947688ecb850706b4ca1804
Paul-Yang-Nicolas <844071188@qq.com>
2024-06-11 23:50:25 +0800

    data

--- auth/src/main/resources/database/data.sql
+++ auth/src/main/resources/database/data.sql
@@ -1,16 +1,11 @@
-DELETE
-FROM
-    USER
-WHERE
-    user_id = 1;
 
-INSERT
-    INTO
-        USER(
-            user_id,
-            user_name
-        )
-    VALUES(
-        1,
-        'Test User'
-    );
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES (1, 'user_1', '123456', 'administrator', '51265901@stu.ecnu.edu.cn', 'ECNU', '2024-06-11 23:30:21');
+INSERT INTO `user` VALUES (2, 'user_2', '123456', 'user', '51265902@stu.ecnu.edu.cn', 'ECNU', '2024-06-11 23:30:21');
+INSERT INTO `user` VALUES (3, 'user_3', '123456', 'user', '51265903@stu.ecnu.edu.cn', 'ECNU', '2024-06-11 23:30:21');
+INSERT INTO `user` VALUES (4, 'user_4', '123456', 'user', '51265904@stu.ecnu.edu.cn', 'ECNU', '2024-06-11 23:30:21');
+INSERT INTO `user` VALUES (5, 'user_5', '123456', 'user', '51265905@stu.ecnu.edu.cn', 'ECNU', '2024-06-11 23:30:21');
+INSERT INTO `user` VALUES (6, 'user_6', '123456', 'user', '51265906@stu.ecnu.edu.cn', 'ECNU', '2024-06-11 23:30:21');
+INSERT INTO `user` VALUES (7, 'user_7', '123456', 'user', '51265907@stu.ecnu.edu.cn', 'ECNU', '2024-06-11 23:30:21');
